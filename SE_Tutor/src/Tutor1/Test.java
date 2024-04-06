5899476c227ce205b7117e7383164358e133643c
VuAnh183 <dinhvuanh2708@gmail.com>
2024-04-06 22:51:49 +0700

    SE_a1_StudentUpdate

--- SE_Tutor/src/Tutor1/Test.java
+++ SE_Tutor/src/Tutor1/Test.java
@@ -2,7 +2,7 @@ package Tutor1;
 
 public class Test {
 	public static void main(String[] args) {
-		Person VuAnh = new Person(1, "Vu Anh");
+		Person VuAnh = new Person(1, "VuAnh");
 		
 		System.out.print(VuAnh);
 	}
