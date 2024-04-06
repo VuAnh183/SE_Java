164dab3cd2a3fb64a98eefaa85711b4b3516f0df
VuAnh183 <dinhvuanh2708@gmail.com>
2024-04-06 16:26:44 +0700

    a1_update

--- SE_Tutor/src/a1_22BI13012/Student.java
+++ SE_Tutor/src/a1_22BI13012/Student.java
@@ -5,7 +5,16 @@ import utils.*;
 /**
  * @overview
  * @attributes
- * 
+ * id				Integer		int
+ * name				String		string 
+ * phoneNumber		String		string
+ * address			String		string
+ * @objects a typical Student object is s=<i, n, p, a>, where id(i), name(n), phoneNumber(p), address(a)
+ * @abstract_properties
+ * mutable(id) = false /\ optional(id) = false /\ min(id) = 1 /\ max(id) = 10^9
+ * mutable(name) = true /\ optional(name) = false /\ length(name) = 50
+ * mutable(phoneNumber) = true /\ optional(phoneNumber) = false /\ length = 10
+ * mutable(address) = true /\ optional(address) = false /\ length = 100
  */
 public class Student {
 
